5509201348ce358a6d5e43abc69d9c627260a3f3
Kelvin Helmut <aek6.io@gmail.com>
2021-09-07 22:12:40 -0500

    2.15 the phonebook - step 7

--- part2 communicating with server/phonebook/src/App.js
+++ part2 communicating with server/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({search, handleSearch}) => {
   return (
@@ -57,15 +57,16 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName, number: newNumber}))
-    setNewName('')
-    setNewNumber('')
+    personService.create({name: newName, number: newNumber})
+      .then(data => {
+        setPersons(persons.concat({name: newName, number: newNumber}))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personService.list().then(data => setPersons(data))
   }, [])
 
   return (
