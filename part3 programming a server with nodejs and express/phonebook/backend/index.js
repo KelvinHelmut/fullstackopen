63c18e5c579d796119f19f1b5e4fd609ad8fffcd
Kelvin Helmut <aek6.io@gmail.com>
2021-10-28 22:05:00 -0500

    3.13 phonebook database - step 1

--- part3 programming a server with nodejs and express/phonebook/backend/index.js
+++ part3 programming a server with nodejs and express/phonebook/backend/index.js
@@ -3,6 +3,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+require('dotenv').config()
+
+const Person = require('./models/person')
+
 app.use(morgan(function (tokens, req, res) {
     let data = [
         tokens.method(req, res),
@@ -20,29 +24,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('static'))
 
-let notes = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/info', (request, response) => {
     response.send(`
         <div>Phonebook has ingo for ${notes.length} people</div>
@@ -52,18 +33,11 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(notes)
+    Person.find({}).then(data => response.json(data))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(data => response.json(data))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -99,5 +73,5 @@ const generateId = () => {
     return Math.floor(Math.random() * 999999999)
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
