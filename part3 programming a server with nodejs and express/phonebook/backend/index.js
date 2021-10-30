d8838e557de4cd72e723e75cb4d0d311d55881b5
Kelvin Helmut <aek6.io@gmail.com>
2021-10-29 22:38:49 -0500

    3.19 phonebook database - step 7

--- part3 programming a server with nodejs and express/phonebook/backend/index.js
+++ part3 programming a server with nodejs and express/phonebook/backend/index.js
@@ -93,6 +93,8 @@ app.use((error, request, response, next) => {
 
     if (error.name == 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name == 'ValidationError') {
+        return response.status(400).send({error: error.message})
     }
 
     next(error)
